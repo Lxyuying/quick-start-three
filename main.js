4bce377df5d7495a9ffa7c42a0d73aca891b5fe0
liaoxingyu <1713468473@qq.com>
2022-08-31 11:15:49 +0800

    feat:建立世界坐标系概念

--- main.js
+++ main.js
@@ -175,7 +175,26 @@ function cylinderMesh(R, h, x, y, z) {
   mesh.position.set(x, y, z)
   return mesh
 }
+// 建立世界坐标系概念
+var mesh = new THREE.Mesh(geometry, material)
+// mesh的本地坐标设置为(50, 0, 0)
+mesh.position.set(50, 0, 0)
+var group = new THREE.Group()
+// group本地坐标设置和mesh一样设置为(50, 0, 0)
+// mesh父对象设置position会影响得到mesh的世界坐标
+group.position.set(150, 0, 0)
+group.add(mesh)
+scene.add(group)
 
+// .position属性获得本地坐标
+console.log('本地坐标', mesh.position)
+
+// getWorldPosition()方法获得世界坐标
+//该语句默认在threejs渲染的过程中执行,如果渲染之前想获得世界矩阵属性、世界位置属性等属性，需要通过代码更新
+scene.updateMatrixWorld(true)
+var worldPosition = new THREE.Vector3()
+mesh.getWorldPosition(worldPosition)
+console.log('世界坐标', worldPosition)
 /**
  * 光源设置
  */
